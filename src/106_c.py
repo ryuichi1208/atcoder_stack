384143e099599a3927542fec512549f37a128441
ryuichi1208 <ryucrosskey@gmail.com>
2019-04-22 16:58:08 +0900

    update

---
+++ src/106_c.py
@@ -0,0 +1,5 @@
+s=input()
+k=int(input())
+
+if (len(s) == 1):
+    print(s)
---
+++ src/115_a.py
@@ -0,0 +1,8 @@
+d=int(input())
+
+n=25-d
+if(d == 0):
+    print("Christmas")
+else:
+    print("Christmas " + "Eve "*n)
+
